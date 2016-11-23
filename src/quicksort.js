f487d1e6dc0658c13d7527eebd81501a5589b452
Florian Keller <github@floriankeller.de>
2016-11-23 20:23:32 +0100

    Add a few tests

--- src/quicksort.js
+++ src/quicksort.js
@@ -2,51 +2,55 @@
 
 import { Utils } from './utils';
 
-const quickSort = (arr, left, right) => {
-    if (!arr instanceof Array || arr.length === 0 || left >= right) {
-        return;
-    }
-
-    // pick the middle and thus the pivot
-    const middle = Math.floor(left + (right - left) / 2);
-    const pivot = arr[middle];
-
-    // Divide into two arrays and presort
-    let i = left;
-    let j = right;
-    while (i <= j) {
-        /*
-         * Find a number on the left side which is greater
-         * than the pivot value, and a number from the
-         * right side which is less than the pivot value.
-         * When the search is complete, we can swap these
-         * numbers.
-         */
-        while (arr[i] < pivot) {
-            i++;
+export const quickSort = () => {
+    const sort = (arr, left, right) => {
+        if (arr.length === 0 || left >= right) {
+            return;
         }
 
-        while (arr[j] > pivot) {
-            j--;
+        // pick the middle and thus the pivot
+        const middle = Math.floor(left + (right - left) / 2);
+        const pivot = arr[middle];
+
+        // Divide into two arrays and presort
+        let i = left;
+        let j = right;
+        while (i <= j) {
+            /*
+             * Find a number on the left side which is greater
+             * than the pivot value, and a number from the
+             * right side which is less than the pivot value.
+             * When the search is complete, we can swap these
+             * numbers.
+             */
+            while (arr[i] < pivot) {
+                i++;
+            }
+
+            while (arr[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                const temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
         }
 
-        if (i <= j) {
-            const temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
+        // recursively sort the rest of the array
+        sort(arr, left, j);
+        sort(arr, i, right);
+    };
+    return {
+        sort (arr) {
+            if (!(arr instanceof Array)) {
+                return null;
+            }
+            sort(arr, 0, arr.length - 1);
+            return arr;
         }
-    }
-
-    // recursively sort the rest of the array
-    quickSort(arr, left, j);
-    quickSort(arr, i, right);
-}
-
-let testArr = [3, 16, 69, 33, 27, 12, 93, 40, 82, 24];
-console.log('Unsorted:', Utils.getPartOfArray(testArr, 10).result);
-console.log(`Array is sorted: ${Utils.isArraySortedAscending(testArr).result}`)
-quickSort(testArr, 0, testArr.length - 1);
-console.log('Sorted:', Utils.getPartOfArray(testArr, 10).result);
-console.log(`Array is sorted: ${Utils.isArraySortedAscending(testArr).result}`)
+    };
+};
