e7de7a63703d5c3233e02ea2e31470015e61536c
Florian Keller <github@floriankeller.de>
2016-11-23 19:47:26 +0100

    Add getPartOfArray + Fixed instanceof checking

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@
 
 export const Utils = {
     isArraySortedAscending (arr) {
-        if (!arr instanceof Array) {
+        if (!(arr instanceof Array)) {
             return false;
         }
         for (let i = 0; i < arr.length - 1; i++) {
@@ -14,5 +14,20 @@ export const Utils = {
             }
         }
         return true;
+    },
+    getPartOfArray (arr, length) {
+        if (!(arr instanceof Array)) {
+            return null;
+        }
+        if (typeof length !== 'number' || length >= arr.length) {
+            // if the given length is bigger or the same size
+            // as the array, return the whole array as string
+            return arr;
+        }
+        let part = "[";
+        for (let i = 0; i < length; i++) {
+            part += arr[i] + ", ";
+        }
+        return part + "... ]";
     }
 };
