cf645316293c9194c07058ab1a50ad4102304979
Florian Keller <github@floriankeller.de>
2016-11-24 02:53:03 +0100

    Capitalize name

--- tests/quicksort.spec.js
+++ tests/quicksort.spec.js
@@ -1,17 +1,17 @@
 'use strict';
 
-import { quickSort } from './quicksort';
+import { QuickSort } from './QuickSort';
 import { utils } from './utils';
 
-describe('quickSort', () => {
+describe('QuickSort', () => {
     it ('sorts 2 numbers', () => {
         const unsortedArray = [5, 3];
-        const sortedArray = quickSort().sort(unsortedArray);
+        const sortedArray = QuickSort().sort(unsortedArray);
         expect(utils.isArraySortedAscending(sortedArray).result).toEqual(true);
     });
     it ('sorts 10 numbers', () => {
         const unsortedArray = [3, 16, 69, 33, 27, 12, 93, 40, 82, 24];
-        const sortedArray = quickSort().sort(unsortedArray);
+        const sortedArray = QuickSort().sort(unsortedArray);
         expect(utils.isArraySortedAscending(sortedArray).result).toEqual(true);
     });
     it ('sorts 100 numbers', () => {
@@ -23,18 +23,18 @@ describe('quickSort', () => {
             143, 90, 134, 337, 278, 76, 463, 88, 450, 178, 459, 255, 393, 398, 237, 336, 321,
             493, 150, 203, 149, 398, 201, 174, 236, 373, 219, 381, 53
         ];
-        const sortedArray = quickSort().sort(unsortedArray);
+        const sortedArray = QuickSort().sort(unsortedArray);
         expect(utils.isArraySortedAscending(sortedArray).result).toEqual(true);
     });
     it ('sorts an empty array', () => {
         const unsortedArray = [];
-        const sortedArray = quickSort().sort(unsortedArray);
+        const sortedArray = QuickSort().sort(unsortedArray);
         expect(sortedArray).toEqual([]);
         expect(utils.isArraySortedAscending(sortedArray).result).toEqual(true);
     });
     it ('doesn\'t sort a null value', () => {
         const unsortedArray = null;
-        const sortedArray = quickSort().sort(unsortedArray);
+        const sortedArray = QuickSort().sort(unsortedArray);
         expect(sortedArray).toEqual(null);
     });
 });
